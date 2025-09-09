c65f798875d4b0e26ce0ef7f307b09d471c09fb9
puchojose87 <puchojose87@gmail.com>
2025-09-09 15:03:37 -0400

    conhecendo um pouco de input

---
+++ recopiladados.py
@@ -0,0 +1,5 @@
+#Todos os dados recopilados por consola ou outros elemnetos por default sao  cadena de caracteres
+#se queremos que se comportem de outra manera, temos que tratar os dados
+nome = input('Me diga seu nome ') #input é utilizado para mostrar un mensagem ao usuario e rcopilar a informação digitada em forma de cadena de caracteres
+edad = int(input('Quantos anos tem '))
+print(f'Voce é {nome} e tem {edad} anos')
