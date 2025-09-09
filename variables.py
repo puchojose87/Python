b55db6383fa0433da52de71a1d896aa5492a2a09
Jose Eugenio <puchojose87@gmail.com>
2025-09-08 22:54:41 -0400

    Arquivo com variables basicas e formatação de str

---
+++ variables.py
@@ -0,0 +1,9 @@
+#arquivo para declarar os tipos basicos de variables 
+nombre='Jose' #cadena de caracteres ou str
+edad=38  #numero entero
+altura=1.60 #numero real ou float
+esAdulto = True #Booleano que solo puede tomar dos valores
+edad='38' #Agora mudo o valor de la variable e o tipo agora é str
+# Agora vamos mostrar algumas das minhas caracteristicas
+# E vamos utilizar uma formatação especial 
+print(f'Mi nombre é {nombre}, tenho {edad} años e tenho uma altura de {altura} metros') 
