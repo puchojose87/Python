baceba7290770314f46511f8c943087533d92a79
Jose Eugenio <puchojose87@gmail.com>
2025-09-10 22:56:12 -0400

    Basico de diccionarios e listas

---
+++ diccionarios.py
@@ -0,0 +1,9 @@
+# Vamos a crear um diccionrio que é uma estrutura de almacenamento de dados
+# que trabalha em pares com uma key e um valor asignado a ela
+# util quando queremos guardar caracteristicas de alguma coisa que não tem comportamento(metodos ou function)
+pessoa = {
+    "nome":'Joao',
+    "edade": 25,
+    "sexo": 'Masculino'
+    }  #Exemplo de um diccionario guardado em una variable pessoa, ele almacena alguns dados
+print(pessoa["nome"]) #imprimimos el valor almacenado en la key nome
---
+++ listas.py
@@ -0,0 +1,3 @@
+frutas = ['maça','cupuaçu','coco','morango'] #Criamos uma lista de frutas 
+# os indices das posiciones iniciam em 0
+print(frutas[1]) #Imprime a segunda fruta
