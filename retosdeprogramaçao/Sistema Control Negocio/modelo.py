274fbc2836927f0196b29e2f6a6c9dd25ad753a9
puchojose87 <puchojose87@gmail.com>
2025-09-25 19:23:28 -0400

    creamos as classes materia prima e produtos

--- retosdeprogramaçao/Sistema Control Negocio/modelo.py
+++ retosdeprogramaçao/Sistema Control Negocio/modelo.py
@@ -1,3 +1,46 @@
 class Materia_Prima:
-    def __init__(self):
-        pass
+    # Construtor da class Materia Prima, receve 3 parametros(nome,valor e quantidade)
+    def __init__(self, nome:str, valor:float, quantidade:int):
+        self.nome=nome
+        self.valor=valor
+        self.quantidade=quantidade
+    # Encapsulamento dos atributos, funções get e set
+    def get_nome(self):
+        return self.nome
+    def set_nome(self, novo_nome:str):
+        self.nome=novo_nome
+    def get_valor(self):
+        return self.valor
+    def set_valor(self, novo_valor:float):
+        self.valor=novo_valor
+    def get_quantidade(self):
+        return self.quantidade
+    def set_quantidade(self,nova_quantidade:int):
+        self.quantidade=nova_quantidade
+
+class Produto:
+    def __init__(self,nome:str,preço:float,quantidade:int,lista_materiais):
+        self.materiais_utilizados[Materia_Prima]=lista_materiais
+        self.nome=nome
+        self.preço=preço
+        self.quantidade=quantidade
+        self.costo=0.0
+        if len(lista_materiais)>0:
+            for material in lista_materiais:
+                self.costo=self.costo+(Materia_Prima(material).get_quantidade()*Materia_Prima(material).get_valor())
+    
+    def get_nome(self):
+        return self.nome
+    def set_nome(self, novo_nome:str):
+        self.nome=novo_nome
+    def get_quantidade(self):
+        return self.quantidade
+    def set_quantidade(self,nova_quantidade:int):
+        self.quantidade=nova_quantidade
+    def get_costo(self):
+        return self.costo
+    def get_preço(self):
+        return self.preço
+    def set_valor(self, novo_preço:float):
+        self.valor=novo_preço
+    
